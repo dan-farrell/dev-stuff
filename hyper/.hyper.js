db396461b1b8fd985c7aaf9fad93cbc00962f01f
Daniel Farrell <farrell.d@live.co.uk>
2020-02-05 09:33:42 +0000

    updates to hyper config

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: 'Fira Mono, Fira Code, Inconsolata, monospace',
+    fontFamily: 'JetBrains Mono, Fira Mono, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
